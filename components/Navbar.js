4256a7c5b0ee353704046d8f292f70c356a1884f
myprojects <shubhamgar4488@gmail.com>
2024-09-28 07:17:46 +0530

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
         <li>Blog</li>
       </ul> */}
 
-      <div className='flex'>
+      <div className='flex '>
       <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Home</button>
       <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Contact us </button>
       <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">About us</button>
